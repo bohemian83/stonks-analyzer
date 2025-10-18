80a51bf583ca0f5c615ad0901d4e4b5d937d95aa
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2025-10-19 00:28:34 +0200

    added layout structure and its css

---
+++ stonks_analyzer/layouts/grid_tcss.py
@@ -0,0 +1,68 @@
+from textual.app import App, ComposeResult
+from textual.containers import Grid, Horizontal, Vertical
+from textual.widgets import Static, Placeholder
+
+
+class GridLayoutWithTCSS(App):
+    CSS_PATH = "grid.tcss"
+
+    def compose(self) -> ComposeResult:
+        yield Vertical(
+            Grid(
+                Placeholder("This is a custom label for p1.", id="p1"),
+                Placeholder("This is a custom label for p1.", id="p2"),
+                Placeholder("This is a custom label for p1.", id="p3"),
+                id="top",
+            ),
+            Grid(
+                Grid(
+                    Placeholder("Left col label 1.", classes="lcl"),
+                    Placeholder("Left col label 2.", classes="lcl"),
+                    Placeholder("Left col label 3.", classes="lcl"),
+                    Placeholder("Left col label 4.", classes="lcl"),
+                    Placeholder("Left col label 5.", classes="lcl"),
+                    Placeholder("Left col label 6.", classes="lcl"),
+                    Placeholder("Left col label 7.", classes="lcl"),
+                    Placeholder("Left col label 8.", classes="lcl"),
+                    id="left-main-col",
+                ),
+                Grid(
+                    Placeholder("Graph goes here", id="graph"),
+                    Grid(
+                        Placeholder("under graph 1", id="ug1"),
+                        Placeholder("under graph 2", id="ug2"),
+                        Placeholder("under graph 3", id="ug3"),
+                        id="under-graph",
+                    ),
+                    id="center-main-col",
+                ),
+                Grid(
+                    Placeholder("Right col label 1.", classes="rcl"),
+                    Placeholder("Right col label 2.", classes="rcl"),
+                    Placeholder("Right col label 3.", classes="rcl"),
+                    Placeholder("Right col label 4.", classes="rcl"),
+                    Placeholder("Right col label 5.", classes="rcl"),
+                    Placeholder("Right col label 6.", classes="rcl"),
+                    Placeholder("Right col label 7.", classes="rcl"),
+                    Placeholder("Right col label 8.", classes="rcl"),
+                    Placeholder("Right col label 9.", classes="rcl"),
+                    Placeholder("Right col label 10.", classes="rcl"),
+                    Placeholder("Right col label 11.", classes="rcl"),
+                    Placeholder("Right col label 12.", classes="rcl"),
+                    id="right-main-col",
+                ),
+                id="main",
+            ),
+            Grid(
+                Placeholder("This is a custom label for p4.", id="p4"),
+                Placeholder("This is a custom label for p5.", id="p5"),
+                Placeholder("This is a custom label for p6.", id="p6"),
+                id="bottom",
+            ),
+            id="content",
+        )
+
+
+if __name__ == "__main__":
+    app = GridLayoutWithTCSS()
+    app.run()
