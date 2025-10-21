b74a09a1ac4b10bac8690a5ed8dc9aab66ccecaf
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2025-10-21 23:48:46 +0200

    added logic to load ticker window, get data, handle exceptions and added some data to static labels

--- stonks_analyzer/analyze_app.py
+++ stonks_analyzer/analyze_app.py
@@ -2,11 +2,16 @@ import plotext as px
 from textual.app import App, ComposeResult
 from textual_plotext import PlotextPlot
 from stonks_analyzer.layouts.stock_dashboard import StockDashboard
+from stonks_analyzer.layouts.ticker_input_screen import TickerInputScreen
 from stonks_analyzer.data.stock_data_builder import get_stock_data
 
 
 class AnalyseApp(App):
     CSS_PATH = "layouts/styles/stock_dashboard_styles.tcss"
+
+    BINDINGS = [
+        ("ctrl+t", "change_ticker", "Change Ticker"),
+    ]
 
     def __init__(self, ticker: str):
         super().__init__()
@@ -17,25 +22,61 @@ class AnalyseApp(App):
         yield StockDashboard()
 
     def on_mount(self) -> None:
-        self.stock_data = get_stock_data(self.ticker)
-        self.update_graph()
-        self.update_labels()
+        self.push_screen(TickerInputScreen(), self.handle_ticker_input)
+
+    def action_change_ticker(self) -> None:
+        self.push_screen(TickerInputScreen(), self.handle_ticker_input)
+
+    def handle_ticker_input(self, ticker: str | None) -> None:
+        if not ticker:
+            self.exit()
+            return
+
+        self.ticker = ticker
+
+        try:
+            self.stock_data = get_stock_data(self.ticker)
+            self.update_graph()
+            self.update_labels()
+
+        except Exception as e:
+            print(f"exception {e} caught")
+            self.push_screen(
+                TickerInputScreen(error_message=str(e)), self.handle_ticker_input
+            )
 
     def update_graph(self) -> None:
-        plt = self.query_one("#graph", PlotextPlot).plt
-        dates = px.datetimes_to_string(self.stock_data.index)
-        plt.candlestick(
-            dates,
-            [
-                self.stock_data["Open"].tolist(),
-                self.stock_data["High"].tolist(),
-                self.stock_data["Low"].tolist(),
-                self.stock_data["Close"].tolist(),
-            ],
+        # plt = self.query_one("#graph", PlotextPlot).plt
+        # dates = px.datetimes_to_string(self.stock_data.index)
+        # plt.candlestick(
+        #     dates,
+        #     [
+        #         self.stock_data["Open"].tolist(),
+        #         self.stock_data["High"].tolist(),
+        #         self.stock_data["Low"].tolist(),
+        #         self.stock_data["Close"].tolist(),
+        #     ],
+        # )
+        # plt.title(f"{self.ticker} Stock Price")
+        pass
+
+    def update_top(self) -> None:
+        self.query_one("#top1").update(
+            f"{self.stock_data['meta']['company_name']} {self.stock_data['ticker']}"
         )
-        plt.title(f"{self.ticker} Stock Price")
+        self.query_one("#top2").update(
+            f"Exchange: {self.stock_data['meta']['exchange']}"
+        )
+        self.query_one("#top3").update(f"Sector: {self.stock_data['meta']['sector']}")
+        self.query_one("#top4").update(
+            f"Price: {round(self.stock_data['price_data'][-1]['close'], 2)}"
+        )
+        self.query_one("#top5").update(
+            f"Market Cap: {self.stock_data['meta']['market_cap']}"
+        )
+        self.query_one("#top6").update(
+            f"P/E: {self.stock_data['valuation_ratios']['pe_ratio']}"
+        )
 
     def update_labels(self) -> None:
-        # Update placeholders with actual data
-        self.query_one("#p1").update(f"Ticker: {self.ticker}")
-        # ... update other labels with stock_data values
+        self.update_top()
